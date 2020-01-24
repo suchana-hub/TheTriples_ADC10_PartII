90d455bf9e98e743326c324194da084b5cb19ebd
Palistha12 <palisthashakya19@gmail.com>
2020-01-24 11:52:12 +0545

    Added Models

--- photos/models.py
+++ photos/models.py
@@ -2,8 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Photos(models.Model):
-    photo=models.ImageField(upload_to='images/')
+    photo=models.ImageField(upload_to='images/',null=True)
     label=models.TextField()
     like=models.IntegerField(default=0)
     caption=models.TextField()
     uploaded_at=models.DateTimeField(auto_now_add=True)
+
+class Album:
+    photos=models.ManyToManyField(Photos)
+    date=models.DateField(auto_now=True)
+    like=models.IntegerChoices(default=0)
+
+
+ 
+
+
